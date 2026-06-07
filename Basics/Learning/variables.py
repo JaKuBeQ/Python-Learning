ba39ec2a87c0210a73c5f850ed88f8c3bbec1910
Jakub <jakub132346@gmail.com>
2026-06-07 18:03:36 +0200

    Organize Basics module, add variable examples, and update IntelliJ project settings

---
+++ Basics/Learning/variables.py
@@ -0,0 +1,10 @@
+ints = 3
+floats = 3.0
+strings  = "hello"
+
+word = str(123)
+word2 = float(123)
+
+print(ints, floats, strings, word, word2)
+print(type(ints), type(floats), type(strings), type(word), type(word2))
+
