9b4ee486fbdab963b2091923f217b7b344993f90
Jakub <jakub132346@gmail.com>
2026-06-08 16:40:47 +0200

    Add conditional examples in loops.py within the Basics module

---
+++ Basics/Learning/loops.py
@@ -0,0 +1,20 @@
+#Equals: a == b
+#Not Equals: a != b
+#Less than: a < b
+#Less than or equal to: a <= b
+#Greater than: a > b
+#Greater than or equal to: a >= b
+
+a = 5
+b = 11
+
+if b > a:
+    print("b is greater than a")
+
+if b == 9:
+    print("b is a 9")
+elif b == 10:
+    print("b is a 10")
+else:
+    print("b is not a 9 or 10")
+
