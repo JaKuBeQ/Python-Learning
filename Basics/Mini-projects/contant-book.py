1481993578405a36ed6e36c10b9c7c17fa9c0e2c
Jakub <jakub132346@gmail.com>
2026-06-10 07:54:10 +0200

    Add contact-book.py mini-project skeleton to Basics module

---
+++ Basics/Mini-projects/contant-book.py
@@ -0,0 +1,11 @@
+contacts = {
+
+}
+
+def add_contact():
+    name = input("Enter name: ")
+    phone = input("Enter phone number: ")
+    email = input("Enter email: ")
+
+def search_contact():
+    
